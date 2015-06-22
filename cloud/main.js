a896f913cd7c017c30b0aeafadc2077d5a93703e
Omar Pera <campbell.sx@gmail.com>
2015-06-22 13:17:53 -0400

    Small changes

--- cloud/main.js
+++ cloud/main.js
@@ -1,7 +1,167 @@
-Parse.Cloud.define("hello", function(request, response) {
-  console.log(request.params);
+Parse.Cloud.define("dropall", function (request, response) {
+    Parse.Cloud.useMasterKey();
 
-  response.success("oh yeah baby 2");
+    var query = new Parse.Query("Room");
+    query.find({
+        success: function (results) {
+            results.forEach(function (result) {
+                result.destroy()
+            })
+            var query = new Parse.Query("UserScore");
+            query.find({
+                success: function (results2) {
+                    results2.forEach(function (result) {
+                        result.destroy()
+                    })
+
+                    var query = new Parse.Query(Parse.User);
+                    query.find({
+                        success: function (results3) {
+                            console.log("lala")
+                            results3.forEach(function (result) {
+                                result.destroy();
+                            })
+                        }
+                    });
+                }
+            });
+        }
+    })
+});
+
+Parse.Cloud.define("dropallinstallations", function (request, response) {
+    Parse.Cloud.useMasterKey();
+
+    var query = new Parse.Query(Parse.Installation);
+    query.find({
+        success: function (results) {
+            results.forEach(function (result) {
+                result.destroy();
+            });
+
+            var query = new Parse.Query(Parse.Session);
+            query.find({
+                success: function (results) {
+                    results.forEach(function (result) {
+                        result.destroy();
+                    })
+                }
+            });
+        }
+    });
+});
+
+Parse.Cloud.define("scaffolding", function (request, response) {
+    // users
+    var usersArray = new Array();
+
+    var ompemi = new Parse.User({
+        username: 'ompemi',
+        password: 'ompemi'
+    });
+    usersArray.push(ompemi);
+
+    var josep = new Parse.User({
+        username: 'josep',
+        password: 'josep'
+    });
+    usersArray.push(josep);
+
+    var pau = new Parse.User({
+        username: 'pau',
+        password: 'pau'
+    });
+    usersArray.push(pau);
+
+    var pocho = new Parse.User({
+        username: 'pocho',
+        password: 'pocho'
+    });
+    usersArray.push(pocho);
+
+    var espinchi = new Parse.User({
+        username: 'espinchi',
+        password: 'espinchi'
+    });
+    usersArray.push(espinchi);
+
+    var numUsers = usersArray.length;
+    usersArray.forEach(function (user) {
+        user.signUp(null, {
+            success: function (list) {
+                numUsers--;
+                if (numUsers == 0) {
+                    // user scores
+                    var userScoresArray = new Array();
+                    var UserScore = Parse.Object.extend("UserScore");
+
+                    var userScoreOmpemi = new UserScore({
+                        user: ompemi,
+                        score: 100
+                    });
+                    userScoresArray.push(userScoreOmpemi);
+
+                    var userScoreJosep = new UserScore({
+                        user: josep,
+                        score: 200
+                    });
+                    userScoresArray.push(userScoreJosep)
+
+                    var userScorePocho = new UserScore({
+                        user: pocho,
+                        score: 300
+                    });
+                    userScoresArray.push(userScorePocho)
+
+
+                    var userScorePau = new UserScore({
+                        user: pau,
+                        score: 1000
+                    });
+                    userScoresArray.push(userScorePau);
+
+                    var userScoreEspinchi = new UserScore({
+                        user: espinchi,
+                        score: 2000
+                    });
+                    userScoresArray.push(userScoreEspinchi);
+
+                    Parse.Object.saveAll(userScoresArray, {
+                        success: function (list) {
+                            // create rooms
+                            var rooms = new Array();
+                            var Room = Parse.Object.extend("Room");
+
+                            var room1 = new Room({
+                                name: "roomWithOmarJosepPocho"
+                            });
+                            var userScoresRoom = room1.relation("scores");
+                            userScoresRoom.add(userScoreOmpemi);
+                            userScoresRoom.add(userScoreJosep);
+                            userScoresRoom.add(userScorePocho);
+                            rooms.push(room1);
+
+                            var room2 = new Room({
+                                name: "roomWithPauEspinchi"
+                            });
+                            var userScoresRoom = room2.relation("scores");
+                            userScoresRoom.add(userScoreEspinchi);
+                            userScoresRoom.add(userScorePau);
+                            rooms.push(room2);
+
+                            Parse.Object.saveAll(rooms, emptyCallbackAndFinish(response));
+                        },
+                        error: function (error) {
+                            response.error(error);
+                        }
+                    });
+                }
+            },
+            error: function (error) {
+                response.error(error);
+            }
+        });
+    })
 });
 
 
@@ -10,7 +170,7 @@ Parse.Cloud.define("hello", function(request, response) {
 // CLOUD: afterUpdate of a Room with a new score added to the relation scores:
 //          get all users of that room except the one who was just added
 //              send them a push notification
-Parse.Cloud.beforeSave("Room", function(request, response) {
+Parse.Cloud.beforeSave("Room", function (request, response) {
     var room = request.object;
     if (room.existed() && (room.dirty('scores') || room.dirty('name'))) { // FIXME: remove name after testing
         // FIXME: decomment me after testing
@@ -35,13 +195,14 @@ Parse.Cloud.beforeSave("Room", function(request, response) {
 //            });
 //        }
 
-        sendPushAdvanced(room, username);
+        sendPushAdvanced(room, username, response);
     } else {
         console.log("We just created a new leaderboard YAY");
+        response.success();
     }
 });
 
-function sendPushAdvanced(room, username) {
+function sendPushAdvanced(room, username, response) {
     // Get the users that were in the room before this new user
     var scoresQuery = room.relation('scores').query();
     scoresQuery.include("user");
@@ -56,37 +217,67 @@ function sendPushAdvanced(room, username) {
                 console.log("Sending push to " + userScore.get('user').get('username'));
             });
 
-            var pushQuery = new Parse.Query(Parse.Installation);
-            pushQuery.containedIn('user', usersArray);
+//            var pushQuery = new Parse.Query(Parse.Installation);
+//            pushQuery.containedIn('user', usersArray);
+//
+//            Parse.Push.send({
+//                where: pushQuery,
+//                data: {
+//                    alert: username + " joined the room " + room.get('name')
+//                }
+//            }, {
+//                success: function() {
+//                    response.success();
+//                },
+//                error: function(error) {
+//                    response.error(error);
+//                }
+//            });
 
-            Parse.Push.send({
-                where: pushQuery,
-                data: {
-                    alert: username + " joined the room " + room.get('name')
-                }
-            }, {
-                success: function() {
-                    // Push was successful
-                },
-                error: function(error) {
-                    // Handle error
-                }
-            });
+            response.success();
         },
         error: function (error) {
             console.log(JSON.stringify(error))
+            response.error(error);
         }
     });
 }
 
-function sendPushSimple(room, username) {
+function sendPushSimple(room, username, response) {
     var scoresQuery = room.relation('scores').query();
     scoresQuery.include('user');
     scoresQuery.find({
         success: function (results) {
-            results.forEach(function (user) {
+            console.log("Notifying that a friend joined to: \n");
+
+            var usersArray = new Array();
+            results.forEach(function (userScore) {
+                usersArray.push(userScore.get('user'));
                 console.log("Tentative push: " + username + " joined the leaderboard " + room.get('name'));
             });
+
+//            var pushQuery = new Parse.Query(Parse.Installation);
+//            pushQuery.containedIn('user', usersArray);
+//
+//            Parse.Push.send({
+//                where: pushQuery,
+//                data: {
+//                    alert: username + " joined the room " + room.get('name')
+//                }
+//            }, {
+//                success: function() {
+//                    response.success();
+//                },
+//                error: function(error) {
+//                    response.error(error);
+//                }
+//            });
+
+            response.success();
+        },
+        error: function (error) {
+            console.log(JSON.stringify(error))
+            response.error(error);
         }
     });
 }
@@ -101,7 +292,7 @@ function sendPushSimple(room, username) {
 //                  if score > oldScore and score <= newScore
 //                      send them a push notification
 
-Parse.Cloud.beforeSave("UserScore", function(request, response) {
+Parse.Cloud.beforeSave("UserScore", function (request, response) {
     var newUserScore = request.object;
 
     // default level
@@ -110,6 +301,7 @@ Parse.Cloud.beforeSave("UserScore", function(request, response) {
         if (!level) {
             newUserScore.set("level", "preRelease1");
         }
+        response.success();
         return;
     }
 
@@ -117,19 +309,19 @@ Parse.Cloud.beforeSave("UserScore", function(request, response) {
     var query = new Parse.Query("UserScore");
     query.include('user');
     query.get(newUserScore.id, {
-        success: function(oldUserScore) {
-            var newScore =  newUserScore.get('score');
+        success: function (oldUserScore) {
+            var newScore = newUserScore.get('score');
             var oldScore = oldUserScore.get('score');
 
             console.log("Old score is " + oldScore + " and new is " + newScore);
 
             // relatedto
-            var roomQuery =  new Parse.Query("Room");
+            var roomQuery = new Parse.Query("Room");
             roomQuery.equalTo('scores', newUserScore);
             roomQuery.find({
                 success: function (rooms) {
                     console.log("Found " + rooms.length + "rooms");
-                    rooms.forEach(function(room) {
+                    rooms.forEach(function (room) {
                         console.log("Room " + room.get('name'));
 
                         var scoresQuery = room.relation('scores').query();
@@ -145,6 +337,8 @@ Parse.Cloud.beforeSave("UserScore", function(request, response) {
                                 });
                             }
                         });
+
+                        response.success();
 //                        var pushQuery = new Parse.Query(Parse.Installation);
 //                        pushQuery.matchesKeyInQuery("user", "user", scoresQuery);
 //                        Parse.Push.send({
@@ -164,12 +358,38 @@ Parse.Cloud.beforeSave("UserScore", function(request, response) {
                 },
                 error: function (error) {
                     console.log(JSON.stringify(error))
+                    response.error(error);
                 }
             });
         },
 
-        error: function(object, error) {
+        error: function (object, error) {
             console.log(JSON.stringify(error))
+            response.error(error);
         }
     });
 });
+
+
+// ----------- helpers ------------- //
+function emptyCallbackAndFinish(response) {
+    return {
+        success: function (list) {
+            response.success();
+        },
+        error: function (error) {
+            console.log(JSON.stringify(error))
+            response.error(error);
+        }
+    }
+}
+
+function emptyCallback() {
+    return {
+        success: function (list) {
+        },
+        error: function (error) {
+            console.log(JSON.stringify(error))
+        }
+    }
+}
